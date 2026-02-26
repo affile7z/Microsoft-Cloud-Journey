99a4a57e4e9a651b447af644f56a7c66124e682d
affile7z <kiwis.contacts2@gmail.com>
2026-02-26 08:29:10 +0200

    Update 01_deploy_base_infra.sh

--- scripts/01_deploy_base_infra.sh
+++ scripts/01_deploy_base_infra.sh
@@ -1,1 +1,22 @@
+#!/bin/bash
 
+# Variables
+RESOURCE_GROUP="RG-Lab-Qatar-01"
+LOCATION="qatarcentral"
+
+# Authentication
+echo "Starting Azure Authentication..."
+az login
+
+# Infrastructure Deployment
+echo "Creating Resource Group: $RESOURCE_GROUP..."
+az group create --name $RESOURCE_GROUP --location $LOCATION \
+    --tags \
+    Project="Cloud-Security-Lab" \
+    Owner="Bogdan" \
+    Environment="Dev" \
+    Certification="AZ104-SC200"
+
+# Verification
+echo "Deployment Validation:"
+az group show --name $RESOURCE_GROUP --output table
